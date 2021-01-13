571614208317ad8460ee9e6294e3f226c7011574
Hexte <lucijan.hrastnik@gmail.com>
2021-01-13 12:55:26 +0100

    added user functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_112525) do
+ActiveRecord::Schema.define(version: 2021_01_13_114524) do
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.integer "price"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+# Could not dump table "posts" because of following StandardError
+#   Unknown type 'reference' for column 'user_id'
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
