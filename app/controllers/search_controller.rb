951fca0e4e55b04f55da7ea3030012484f469f3b
Hexte <lucijan.hrastnik@gmail.com>
2021-03-02 23:05:04 +0100

    minor fixes

--- app/controllers/search_controller.rb
+++
@@ -1,9 +0,0 @@
-class SearchController < ApplicationController
-  def index
-    @query = params[:query]
-
-    @q = Post.ransack(params[:query])
-    @posts = @q.result(distinct: true)
-
-  end
-end
